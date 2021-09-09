d8ef0edc76731d98ee75a4443b30699c55537453
Gustavo Amorim <gamorim@informa.com.br>
2021-09-09 10:57:59 -0300

    make it better

---
+++ app.ts
@@ -0,0 +1,25 @@
+import Express from "express";
+import { init } from "../ppt-api/scr/puppeteer";
+
+const app = Express();
+
+const PORT = 8001;
+
+export class Results {
+  date?: Date;
+  results: any[] = [];
+}
+
+app.get("/allresults", async (req: any, res: any) => {
+  try {
+    res.status(200).json(await init());
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+
+  res.send("sincronizando... isto pode levar alguns segundos");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
